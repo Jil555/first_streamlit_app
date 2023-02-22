7b3a3aa79458bae9d28b56edb822af6bc8f7dcef
Jil555 <125989625+Jil555@users.noreply.github.com>
2023-02-22 12:28:39 +0530

    Update streamlit_app.py
    changed as per Badge initiative, mentioned code in Nadge

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner with Love')
 streamlit.header ('Love Dish')
 streamlit.text('Love Love Love')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
